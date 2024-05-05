efd8b88b73cf4f684c84b3473ec85fd68fcf525b
name <yourgithub@email.com>
2024-05-05 06:28:17 -0400

    week3_4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
+    
    # path('login/', TemplateView.as_view(template_name="index.html")),
     # path(route='login', view=views.login_user, name='login'),
 
